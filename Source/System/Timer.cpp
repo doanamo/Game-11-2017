12575e7fc07187ef5173599cbe06807cf297f14b
Piotr Doan <doanpiotr@gmail.com>
2018-01-31 19:25:06 +0100

    Improve documentation in Timer class.

--- Source/System/Timer.cpp
+++ Source/System/Timer.cpp
@@ -8,8 +8,8 @@ Timer::Timer() :
     m_previousTimeCounter(m_currentTimeCounter),
     m_maxFrameDeltaSeconds(std::numeric_limits<float>::max())
 {
-    // Check if timer's frequency is valid.
-    // Assertion's failure here may indicate improperly initialized platform code.
+    // Check if the timer's frequency is valid.
+    // Assertion's failure here may indicate improperly initialized GLFW library.
     Verify(m_timerFrequency != 0, "Failed to retrieve a valid timer frequency!");
 }
 
@@ -19,44 +19,48 @@ Timer::~Timer()
 
 void Timer::Reset()
 {
-    Assert(m_timerFrequency != 0);
+    Assert(m_timerFrequency != 0, "Timer frequence is invalid!");
 
+    // Reset internal timer values.
     m_currentTimeCounter = glfwGetTimerValue();
     m_previousTimeCounter = m_currentTimeCounter;
 }
 
 void Timer::Tick()
 {
-    Assert(m_timerFrequency != 0);
+    Assert(m_timerFrequency != 0, "Timer frequence is invalid!");
 
+    // Remember time points of the two last ticks.
     m_previousTimeCounter = m_currentTimeCounter;
     m_currentTimeCounter = glfwGetTimerValue();
 }
 
 float Timer::CalculateFrameDelta()
 {
-    Assert(m_timerFrequency != 0);
+    Assert(m_timerFrequency != 0, "Timer frequence is invalid!");
 
     // Calculate elapsed time since the last frame.
     uint64_t elapsedTimeCounter = m_currentTimeCounter - m_previousTimeCounter;
 
-    // Calculate frame time delta in seconds.
+    // Calculate frame time delta between last two ticks in seconds.
     float frameDeltaSeconds = static_cast<float>(elapsedTimeCounter * (1.0 / m_timerFrequency));
 
-    // Clamp delta value.
+    // Clamp delta value between 0.0f and set maximum value.
     frameDeltaSeconds = std::max(0.0f, frameDeltaSeconds);
     frameDeltaSeconds = std::min(frameDeltaSeconds, m_maxFrameDeltaSeconds);
 
-    // Return calculated delta value.
+    // Return calculated frame delta value.
     return frameDeltaSeconds;
 }
 
 void Timer::SetMaxFrameDelta(float value)
 {
+    // Set maximum frame delta allowed.
     m_maxFrameDeltaSeconds = value;
 }
 
 float Timer::GetMaxFrameDelta() const
 {
+    // Return the set maximum frame delta.
     return m_maxFrameDeltaSeconds;
 }
--- Source/System/Timer.hpp
+++ Source/System/Timer.hpp
@@ -6,19 +6,26 @@
     Timer
 
     Keeps track of time and provides utilities such as automatic
-    calculation of delta time between ticks or frame rate.
+    calculation of delta time between ticks and frame rate measurement.
 
-    Exmaple usage:
+    void ExampleTimerUsage()
+    {
+        // Create a timer instance.
+        // This also makes the timer tick once.
         System::Timer timer;
 
+        // Run a loop that will measure delta time.
         while(true)
         {
+            // Calculate delta time between two last ticks.
             float dt = timer.CalculateFrameDelta();
+            Log() << "Current delta time: " << dt;
 
-            // ...
-
+            // Perform some calculations over a frame and tick the timer.
+            Sleep(1000);
             timer.Tick();
         }
+    }
 */
 
 namespace System
@@ -36,20 +43,22 @@ namespace System
         // Ticks the timer.
         void Tick();
 
-        // Calculates frame delta time in seconds between last two ticks.
+        // Calculates a frame delta time in seconds between last two ticks.
         float CalculateFrameDelta();
 
-        // Sets maximum frame delta in seconds that can be returned.
+        // Sets the maximum frame delta in seconds that can be returned.
         void SetMaxFrameDelta(float value);
 
-        // Gets maximum frame delta in seconds that can be returned.
+        // Gets the maximum frame delta in seconds that can be returned.
         float GetMaxFrameDelta() const;
 
     private:
+        // Internal timer values.
         uint64_t m_timerFrequency;
         uint64_t m_currentTimeCounter;
         uint64_t m_previousTimeCounter;
 
+        // Maximum accumulated frame delta.
         float m_maxFrameDeltaSeconds;
     };
 }
