db6fb161b73cd8bc00a92c38e7e4354ba651bb12
Piotr Doan <doanpiotr@gmail.com>
2018-02-07 15:19:30 +0100

    Review the use of asserts in Timer class.

--- Source/System/Timer.cpp
+++ Source/System/Timer.cpp
@@ -9,7 +9,7 @@ Timer::Timer() :
     m_maxFrameDeltaSeconds(std::numeric_limits<float>::max())
 {
     // Check if the timer's frequency is valid.
-    // Assertion's failure here may indicate improperly initialized GLFW library.
+    // Assertion's failure may indicate improperly initialized GLFW library.
     Verify(m_timerFrequency != 0, "Failed to retrieve a valid timer frequency!");
 }
 
@@ -19,7 +19,7 @@ Timer::~Timer()
 
 void Timer::Reset()
 {
-    Assert(m_timerFrequency != 0, "Timer frequence is invalid!");
+    Assert(m_timerFrequency != 0, "Timer frequency is invalid!");
 
     // Reset internal timer values.
     m_currentTimeCounter = glfwGetTimerValue();
@@ -28,7 +28,7 @@ void Timer::Reset()
 
 void Timer::Tick()
 {
-    Assert(m_timerFrequency != 0, "Timer frequence is invalid!");
+    Assert(m_timerFrequency != 0, "Timer frequency is invalid!");
 
     // Remember time points of the two last ticks.
     m_previousTimeCounter = m_currentTimeCounter;
@@ -37,17 +37,17 @@ void Timer::Tick()
 
 float Timer::CalculateFrameDelta()
 {
-    Assert(m_timerFrequency != 0, "Timer frequence is invalid!");
+    Assert(m_timerFrequency != 0, "Timer frequency is invalid!");
 
     // Calculate elapsed time since the last frame.
+    Assert(m_currentTimeCounter >= m_previousTimeCounter, "Previous time counter is higher than the current time counter!");
     uint64_t elapsedTimeCounter = m_currentTimeCounter - m_previousTimeCounter;
 
     // Calculate frame time delta between last two ticks in seconds.
     float frameDeltaSeconds = static_cast<float>(elapsedTimeCounter * (1.0 / m_timerFrequency));
 
     // Clamp delta value between 0.0f and set maximum value.
-    frameDeltaSeconds = std::max(0.0f, frameDeltaSeconds);
-    frameDeltaSeconds = std::min(frameDeltaSeconds, m_maxFrameDeltaSeconds);
+    frameDeltaSeconds = Utility::Clamp(frameDeltaSeconds, 0.0f, m_maxFrameDeltaSeconds);
 
     // Return calculated frame delta value.
     return frameDeltaSeconds;
@@ -55,10 +55,14 @@ float Timer::CalculateFrameDelta()
 
 void Timer::SetMaxFrameDelta(float value)
 {
+    Assert(m_timerFrequency != 0, "Timer frequency is invalid!");
+
     m_maxFrameDeltaSeconds = value;
 }
 
 float Timer::GetMaxFrameDelta() const
 {
+    Assert(m_timerFrequency != 0, "Timer frequency is invalid!");
+
     return m_maxFrameDeltaSeconds;
 }
